056ada332f716ae2195a54dc63955ec267f22cf3
tas2580 <tobi.schaefer@gmail.com>
2016-11-23 02:21:39 +0100

    test version

--- tests/functional/phpbb_functional_test.php
+++ tests/functional/phpbb_functional_test.php
@@ -10,6 +10,7 @@ class phpbb_functional_test extends \tas2580\opensearch\tests\base\functional_te
 		parent::setUp();
 		$this->purge_cache();
 		$this->login();
+		$this->admin_login();
 	}
 
 	public function test_version_check()
@@ -22,6 +23,6 @@ class phpbb_functional_test extends \tas2580\opensearch\tests\base\functional_te
 		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=details&ext_name=tas2580%2Fopensearch&sid=' . $this->sid);
 		// Assert extension is up to date
 		$this->assertGreaterThan(0, $crawler->filter('.successbox')->count());
-		$this->assertContains($this->lang('UP_TO_DATE', 'Pages'), $crawler->text());
+		$this->assertContains($this->lang('UP_TO_DATE', 'Opensearch'), $crawler->text());
 	}
 }
