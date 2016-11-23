2b3657e998fa8d4f6ce2371558d0a326afac6604
tas2580 <tobi.schaefer@gmail.com>
2016-11-23 02:16:25 +0100

    test version

--- tests/functional/phpbb_functional_test.php
+++ tests/functional/phpbb_functional_test.php
@@ -12,8 +12,16 @@ class phpbb_functional_test extends \tas2580\opensearch\tests\base\functional_te
 		$this->login();
 	}
 
-    public function test_opensearch()
-    {
-		$crawler = $this->request('GET', 'app.php/opensearch.xml');
-    }
+	public function test_version_check()
+	{
+		// Log in to the ACP
+		$this->login();
+		$this->admin_login();
+		$this->add_lang('acp/extensions');
+		// Load the Pages extension details
+		$crawler = self::request('GET', 'adm/index.php?i=acp_extensions&mode=main&action=details&ext_name=tas2580%2Fopensearch&sid=' . $this->sid);
+		// Assert extension is up to date
+		$this->assertGreaterThan(0, $crawler->filter('.successbox')->count());
+		$this->assertContains($this->lang('UP_TO_DATE', 'Pages'), $crawler->text());
+	}
 }
