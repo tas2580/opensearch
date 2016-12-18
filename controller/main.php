ebc37785ad738dfc25f4d7e3998f7eac4a329980
tas2580 <tobi.schaefer@gmail.com>
2016-12-18 03:44:54 +0100

    test

--- controller/main.php
+++ controller/main.php
@@ -43,6 +43,8 @@ class main
 	*/
 	public function xml()
 	{
+
+		
 		$xml = '<?xml version="1.0" encoding="UTF-8"?>';
 		$xml .= '<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">';
 		$xml .= '<ShortName>' . $this->config['sitename'] . '</ShortName>';
